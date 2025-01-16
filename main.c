c5a15ffa7aff0c39f67b7145e3730c33a6c8c229
Hetya <henrialsace@laposte.net>
2025-01-16 05:40:44 +0100

    ft_write: add the error handling

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+// #include <error.h>
+// #include <stdlib.h>
 
 ssize_t ft_read(int fd, void *buf, size_t count);
 int strcmp(const char *s1, const char *s2);
@@ -14,4 +16,5 @@ int main(){
     // ft_strlen();
     char *test_str = "Hello World!\n";
     printf("ret : %ld, should return : %ld\n",ft_write(1, test_str, strlen(test_str)), write(1, test_str, strlen(test_str)));
+    printf("ret : %ld, should return : %ld\n",ft_write(-1, test_str, strlen(test_str)), write(-1, test_str, strlen(test_str)));
 }
--- src/ft_write.s
+++ src/ft_write.s
@@ -1,18 +1,38 @@
+extern __errno_location
+; section .text
 global ft_write
-section .text
 
 ft_write:
-    push rbp ; Save the old base pointer(rbp) onto the stack
-    mov rbp, rsp; Set the base pointer to the current stack pointer
+    ; setup the stack frame (not mandatory in this case)
+    push rbp        ; Save the old base pointer(rbp) onto the stack
+    mov rbp, rsp    ; Set the base pointer to the current stack pointer
 
-    mov rax, 0x01    ; read syscall
+    mov rax, 0x01   ; read syscall
     ; Moving parameters to the right registers is not needed since there are already in the right registers
     ; mov rdi, rdi
     ; mov rsi, rsi
     ; mov rdx, rdx
-    syscall ; Make the system call
+    syscall         ; Make the system call
     ; Same here we don't need to move the return value to rax since the syscall already does it
 
-    pop rbp ; Restore the old base pointer (rbp) from the stack
+    ; error handling
+    test rax, rax
+    js error
 
+    pop rbp         ; Restore the old base pointer (rbp) from the stack
+
+    ret
+    
+error:
+    ; setup the stack frame (not mandatory in this case)
+    push rbp
+    mov rbp, rsp
+
+    neg rax     ; make the error value absolute
+    mov rdi, rax        ; copy the syscall error value in rdi
+    call __errno_location       ; get the errno
+    mov [rax], rdi      ; put the copy of the syscall return at the address of errno
+    mov rax, -1     ; set the return value to -1
+
+    pop rbp
     ret
